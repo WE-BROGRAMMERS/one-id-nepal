a64c24c9b251dcbe59e7471a221df2255919eccd
dahalutsab <dlutsab2120@gmail.com>
2026-07-10 15:07:47 +0545

    create initial setup for api gateway

---
+++ api_gateway/src/test/java/com/one_id_nepal/api_gateway/ApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.one_id_nepal.api_gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
